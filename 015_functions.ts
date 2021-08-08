f46485eb1ba5575c67bd56ee59aa54250b682e88
Brian Kozub <brikozub@gmail.com>
2021-08-07 22:15:37 -0600

    Completed Functions

---
+++ 015_functions.ts
@@ -0,0 +1,27 @@
+export {}
+
+// function fullName (firstName : string, lastName: string) {
+//     return firstName + ' ' + lastName;
+// }
+
+// console.log(fullName('Brian', 'Kobuz'));
+
+function gradeGenerator(grade: number) : string {
+    if (grade >= 90) {
+        return 'A';
+    } else if (grade >= 80) {
+        return 'B';
+    } else if (grade >= 70) {
+        return 'C';
+    } else if (grade >= 60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+console.log(gradeGenerator(90));
+console.log(gradeGenerator(85));
+console.log(gradeGenerator(72));
+console.log(gradeGenerator(60));
+console.log(gradeGenerator(17));
---
+++ dist/015_functions.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// function fullName (firstName : string, lastName: string) {
+//     return firstName + ' ' + lastName;
+// }
+// console.log(fullName('Brian', 'Kobuz'));
+function gradeGenerator(grade) {
+    if (grade >= 90) {
+        return 'A';
+    }
+    else if (grade >= 80) {
+        return 'B';
+    }
+    else if (grade >= 70) {
+        return 'C';
+    }
+    else if (grade >= 60) {
+        return 'D';
+    }
+    else {
+        return 'F';
+    }
+}
+console.log(gradeGenerator(90));
+console.log(gradeGenerator(85));
+console.log(gradeGenerator(72));
+console.log(gradeGenerator(60));
+console.log(gradeGenerator(17));
