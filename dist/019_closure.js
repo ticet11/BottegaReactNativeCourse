bdc55c3c491f0e184fc9af6bb1742620a237c8af
Brian Kozub <brikozub@gmail.com>
2021-08-08 17:57:37 -0600

    Completed Intro to Closures

---
+++ dist/019_closure.js
@@ -0,0 +1,41 @@
+"use strict";
+// functions have access to any public variables in outer scope
+// function nameFunc(name: string): void {
+// 	var n: string = name;
+// 	function printName() {
+// 		console.log(n);
+// 	}
+// 	printName();
+// }
+// nameFunc("Brian Kozub");
+/* --------------------------------------- */
+// Inner function maintains access to outer scope even AFTER values are returned
+// function nameFunc(name: string) {
+// 	var n: string = name;
+// 	return function () {
+// 		console.log(n);
+// 	};
+// }
+// var nameAgain = nameFunc("Brian Kozub");
+// nameAgain();
+/* --------------------------------------- */
+function lineup() {
+    var nowBatting = 1;
+    return {
+        nextBatter() {
+            nowBatting++;
+        },
+        currentBatter() {
+            return nowBatting;
+        },
+    };
+}
+let batters = lineup();
+let pitchers = lineup();
+while (batters.currentBatter() <= 20) {
+    if (batters.currentBatter() === 11 || batters.currentBatter() === 1) {
+        console.log('Pitcher: ' + pitchers.currentBatter());
+    }
+    console.log('Batter: ' + batters.currentBatter());
+    batters.nextBatter();
+}
