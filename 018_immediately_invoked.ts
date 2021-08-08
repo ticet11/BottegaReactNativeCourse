8635489ed834ccdbf309e06cd923a938aead43d9
Brian Kozub <brikozub@gmail.com>
2021-08-08 17:01:00 -0600

    Completed Immediately Invoked Functions

---
+++ 018_immediately_invoked.ts
@@ -0,0 +1,11 @@
+export {}
+
+let names : string[] = ['Brian', 'Dan', 'Django', 'Bango'];
+let i : number = 0;
+
+(function() {
+    for (i; i < names.length; i++) {
+        console.log(i);
+    }
+    console.log('Final: ' + i);
+})();
---
+++ dist/018_immediately_invoked.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = ['Brian', 'Dan', 'Django', 'Bango'];
+let i = 0;
+(function () {
+    for (i; i < names.length; i++) {
+        console.log(i);
+    }
+    console.log('Final: ' + i);
+})();
