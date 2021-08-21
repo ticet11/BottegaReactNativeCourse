68ecade32efaa4b0b6e87fe3e0a6dfff10f8b4b0
Brian Kozub <brikozub@gmail.com>
2021-08-21 13:11:10 -0600

    Completed Promises in TypeScript

---
+++ 029_promises.ts
@@ -0,0 +1,27 @@
+// Start mowing -> Pending
+// Complete mowing process -> Resolve
+// Did not complete mowing process -> Reject
+
+let performUpload = function (imgStatus): Promise<{ imgStatus: string }> {
+	return new Promise((resolve) => {
+        console.log(`Status: ${imgStatus}`);
+        setTimeout(() => {
+			resolve({ imgStatus: imgStatus });
+		}, 1000);
+	});
+};
+
+var upload;
+var compress;
+var transfer;
+
+performUpload('uploading...').then((res) => {
+    upload = res;
+    return performUpload('compressing...');
+}).then((res) => {
+    compress = res;
+    return performUpload('transferring...');
+}).then((res) => {
+    transfer = res;
+    return performUpload('Upload Complete.')
+})
--- dist/028_higher_order_callbacks.js
+++ dist/028_higher_order_callbacks.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let dbQuery = function () {
-    setTimeout(() => console.log('query results'), 3000);
+    setTimeout(() => 'query results', 3000);
 };
 function loadPage(q) {
     console.log('header');
